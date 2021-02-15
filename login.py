edb63e41bb961663beeb04f2828e8127adfc5179
denis <mrisholigare@gmail.com>
2021-02-15 14:23:17 +0300

    more workings on main.py

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 # creating a class,it creates the blueprint for the pearson who will use the susyem
 class logins:
-   user_list =[]
+   user_info =[]
    def __init__(self,user_credentials,secret_key):
     '''
     instantiation
@@ -10,4 +10,4 @@ class logins:
     self.secret_key = secret_key
     # the users details have to be saved somewhere,here we have to create a fuction to help us do so
    def save_input(self):
-       logins.user_list.append(self)
+       logins.user_info.append(self)
--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 #  provides access to functions that support these types of operations
 import random
 # importing user class form login file
-from login import User_login
+from login import logins
 
 # main function 
 def main():
     while True:
        print("welcome to password locker")
        print("\n")
-       print("what do you want to do?")
+       print("what do you want to do?create account,sign in or exit?type your answer below")
 # created a dummy variable that holds any inputs that the user will choose
        detail_holder = input()
       
        print("\n")
        if detail_holder == "sign-in":
-          print('create username')
+          print('enter username')
           username_created = input()
 
-          print("create password")
+          print("create secret-key")
           password_created = input()
 
-          print("confirm password")
+          print("confirm secret-key")
           password_confirmed = input()
 # confirming if the password entered was right
           while password_created != password_confirmed:
-              print('wrong password created')
+              print('invalid key entered')
               print('re-enter your password please')
               password_reentered = input()
               print("confirm your password")
