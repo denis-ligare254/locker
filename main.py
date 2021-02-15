2808f6fb3fd72effceb8fe356b1a70b1e95f8b86
denis <mrisholigare@gmail.com>
2021-02-15 12:02:32 +0300

    main function added under main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import random
 # importing user class form login file
 from login import User_login
 
+
+def main():
+    while True:
+       print("welcome to password locker")
+       print("\n")
+       print("what do you want to do?")
+       activity = input()
+
