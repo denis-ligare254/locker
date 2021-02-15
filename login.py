193778971c6f9031f90beecb284ff10b111cee59
denis <mrisholigare@gmail.com>
2021-02-15 11:47:34 +0300

    initialization for def __init__

--- login.py
+++ login.py
@@ -2,4 +2,9 @@
 class User_login:
  # user details to be stored here
    user_details=[]
-   
+   # function to contain the details
+
+   def _init_(self, user_name, user_password):
+      self.user_name = user_name
+      self.user_password = user_password
+
